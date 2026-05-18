989d3206ebe02bbbe5343e9f74754fced7a430e4
Shivam kumar mahato <shivammahato321@gmail.com>
2026-05-18 16:04:10 +0530

    Initial commit: add project files and .gitignore

---
+++ Increment_Decrement.js
@@ -0,0 +1,13 @@
+let followers =999
+followers++;
+console.log(followers);
+
+let tickets = 50
+tickets--;
+console.log(tickets);
+
+let productPrice = 2000
+let gst = 18
+let gstAmount = productPrice * 18/100
+let finalPrice = productPrice + gstAmount
+console.log("The final price of the product is: $" + finalPrice);
---
+++ addition.js
@@ -0,0 +1,5 @@
+let laptopPrice = 1200;
+let mousePrice = 25;
+let keyboardPrice = 75;
+let totalPrice = laptopPrice + mousePrice + keyboardPrice;
+console.log("The total price is: $" + totalPrice);
